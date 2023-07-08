ec056f8cefe8324d70f6c787169d136827c221c4
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-07-08 18:51:40 +0330

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1 class="text-3xl font-bold mb-2">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="postcss">
+	:global(html) {
+		padding: theme(spacing.4);
+		background-color: theme(colors.gray.200);
+	}
+</style>
