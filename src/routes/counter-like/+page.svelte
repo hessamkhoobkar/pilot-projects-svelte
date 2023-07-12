e2f7e0628226b828a22606b9bb354b42fa498241
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-07-12 18:09:25 +0330

    add pulse effect

--- src/routes/counter-like/+page.svelte
+++ src/routes/counter-like/+page.svelte
@@ -44,6 +44,19 @@
 								d="M12 20.325q-.35 0-.713-.125t-.637-.4l-1.725-1.575q-2.65-2.425-4.788-4.813T2 8.15Q2 5.8 3.575 4.225T7.5 2.65q1.325 0 2.5.562t2 1.538q.825-.975 2-1.538t2.5-.562q2.35 0 3.925 1.575T22 8.15q0 2.875-2.125 5.275T15.05 18.25l-1.7 1.55q-.275.275-.637.4t-.713.125Z"
 							/>
 						</svg>
+
+						<svg
+							class="pulse"
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+						>
+							<path
+								fill="currentColor"
+								d="M12 20.325q-.35 0-.713-.125t-.637-.4l-1.725-1.575q-2.65-2.425-4.788-4.813T2 8.15Q2 5.8 3.575 4.225T7.5 2.65q1.325 0 2.5.562t2 1.538q.825-.975 2-1.538t2.5-.562q2.35 0 3.925 1.575T22 8.15q0 2.875-2.125 5.275T15.05 18.25l-1.7 1.55q-.275.275-.637.4t-.713.125Z"
+							/>
+						</svg>
 					{:else}
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
 							<path
@@ -197,18 +210,82 @@
 
 				span {
 					margin-right: 0.5rem;
+					position: relative;
+				}
+
+				svg {
+					transition: transform 0.5s ease-in-out;
+
+					&.pulse {
+						position: absolute;
+						top: 0;
+						transform: scale(0.5, 0.5);
+						color: white;
+					}
 				}
 
 				&:hover {
 					color: #a50021;
 					border-color: #a50021;
 					background: hsl(348, 54%, 96%);
+					svg {
+						transition: transform 0.5s ease-in-out;
+						transform: scale(1.3, 1.3);
+					}
+				}
+				&:active {
+					svg {
+						transition: transform 0.1s ease-in-out;
+						transform: scale(0.9, 0.9);
+					}
 				}
 
 				&.liked {
-					color: #a50021;
+					color: #fff;
+					border-color: #a50021;
+					background-color: #a50021;
+
+					svg.pulse {
+						animation: pulse 1s forwards ease-out;
+						box-shadow: 0px 15px 28px -11px rgba(255, 79, 93, 0.9);
+					}
 				}
 			}
+		}
+	}
+
+	@-webkit-keyframes pulse {
+		0% {
+			opacity: 1;
+		}
+		70% {
+			opacity: 0;
+			transform: scale(8, 8);
+		}
+		99% {
+			opacity: 0;
+			transform: scale(8, 8);
+		}
+		100% {
+			opacity: 0;
+			transform: scale(0.5, 0.5);
+		}
+	}
+	@keyframes pulse {
+		0% {
+			opacity: 1;
+		}
+		70% {
+			opacity: 0;
+			transform: scale(8, 8);
+		}
+		99% {
+			opacity: 0;
+			transform: scale(8, 8);
+		}
+		100% {
+			opacity: 0;
+			transform: scale(0.5, 0.5);
 		}
 	}
 </style>
